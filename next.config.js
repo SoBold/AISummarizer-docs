1b00adcd2ba01b8dcfd899e756fe31edc9fd9b2e
soboldsantosh <santosh@soboldltd.com>
2024-09-19 14:51:11 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const withNextra = require("nextra")({
-    theme: "nextra-theme-docs",
-    themeConfig: "./theme.config.tsx",
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.tsx',
 });
 
-const isProduction = process.env.NODE_ENV === "production";
-const assetPrefix = isProduction ? "/AISummarizer-docs" : "";
+const isProduction = process.env.NODE_ENV === 'production';
+const assetPrefix = isProduction ? '/AISummarizer-docs' : '';
 
 const nextConfig = {
     images: {
